c9af712e5fc0d1116f1556fab6ffd878ffd34183
LeviMaycon <levioficial361@gmail.com>
2025-01-11 21:31:33 -0300

    feat: create hero component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { HeroComponent } from "./components/hero/hero.component";
+import { MemorialWallComponent } from "./components/memorial-wall/memorial-wall.component";
+import { AboutusComponent } from "./components/aboutus/aboutus.component";
+import { TestimonialsComponent } from "./components/testimonials/testimonials.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, RouterModule, AboutusComponent, TestimonialsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
